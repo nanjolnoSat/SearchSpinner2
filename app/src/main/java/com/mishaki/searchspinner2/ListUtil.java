d6e82058432f8bd8949853059b14b30657dc1132
mishaki <mishaki@ganguo.hk>
2020-09-01 15:38:54 +0800

    完成改版

---
+++ app/src/main/java/com/mishaki/searchspinner2/ListUtil.java
@@ -0,0 +1,8 @@
+package com.mishaki.searchspinner2;
+
+/**
+ * Created by 杜壁奇<br/>
+ * on 2020/09/01 14:01
+ */
+public class ListUtil {
+}
--- app/src/main/java/com/mishaki/searchspinner2/mode/SpinnerContentMode.kt
+++
@@ -1,8 +0,0 @@
-package com.mishaki.searchspinner2.mode
-
-interface SpinnerContentMode<T> {
-    /**
-     * 根据选择到的对象返回想要显示的内容
-     */
-    fun getContent(t: T): String
-}
